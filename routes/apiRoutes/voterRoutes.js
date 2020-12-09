a4377df71a8263ae983cb2e0f4553e02d29c508a
Anthony H Kieu <anthonykieu1@Anthonys-MBP.fios-router.home>
2020-12-08 19:30:14 -0500

    fixed votes table

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -90,8 +90,8 @@ router.put('/voter/:id', (req, res) => {
 
 router.delete('/voter/:id', (req, res) => {
     const sql = `DELETE FROM voters WHERE id = ?`;
-
-    db.run(sql, req.params.id, function (err, result) {
+    const params = [req.params.id];
+    db.run(sql, params.id, function (err, result) {
         if (err) {
             res.status(400).json({ error: res.message });
             return;
